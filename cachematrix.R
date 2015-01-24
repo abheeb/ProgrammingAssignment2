162e680dfd03e34c56e2f1a08e3747d8a2e311bd
AbheeB <abhee.b@gmail.com>
2015-01-24 10:52:30 -0600

    First attempt to commit the full solution to github

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Functions below are very similar to the example functions given in the assignment.
+## Though this is my formal assignment submission I don't like following things regarding
+## this approach.  1. This is a muzzle laoding gun type approach.  Everytime you change matrix
+## You will have to fire MakeCacheMatrix so that m resets to NULL.  It doesn't automatically detect 
+## change in the original matrix file.  Anyway...
 
-## Write a short comment describing this function
+## makeCacheMatrix function will take in given matrix and produce list as given below
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL      
+    }
+    get <- function() x
+    setmean <- function(a) m <<- a
+    getmean <- function() m
+    list(set = set, get = get, setmean = setmean, getmean = getmean)
 }
 
 
-## Write a short comment describing this function
+## This function will check the value of M. if it is null then it means inverse was never calculated.
+## so it will calculate inverse, store it in veriable m and return m.  if the value of m is not null then it means
+## it already has inverse sotred in it.  So then it will display that value along with message indicating that it's a cached data
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    m <- x$getmean()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setmean(m)
+    m
 }
